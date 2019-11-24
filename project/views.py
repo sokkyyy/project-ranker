77b4909af01af194389b033aaee89977df035bb7
sokkyyy <ndegwaraymond@gmail.com>
2019-11-24 17:06:24 +0300

    Added the logic for allowing users to register to application

--- project/views.py
+++ project/views.py
@@ -1,10 +1,27 @@
 from django.shortcuts import render
 from .forms import RegForm
+from django.contrib.auth.models import User
 # Create your views here.
 def home(request):
     return render(request,'home.html')
 
 def register(request):
+    if request.method == 'POST':
+        form = RegForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            email = form.cleaned_data['email']
+            first_name = form.cleaned_data['first_name']            
+            last_name = form.cleaned_data['last_name']
+
+            user = User.objects.create_user(username,email,password)
+            user.first_name = first_name
+            user.last_name = last_name
+            user.save()
+            
+            
     form = RegForm()
     return render(request,'auth/registration.html',
     {"form":form})
+de
