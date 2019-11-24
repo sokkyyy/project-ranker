73cdaabf97576b193ef6cfeefe0d39643120a7e7
sokkyyy <ndegwaraymond@gmail.com>
2019-11-24 14:21:48 +0300

    Added the profile model for the user. Installed pillow to manage uploading profile pictures and the settings to handle serving to the webpage

--- project/models.py
+++ project/models.py
@@ -1,5 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
-
+class Profile(models.Model):
+    profile_pic = models.ImageField(upload_to='profile_pics/', default='avatar.png')
+    bio = models.TextField(default='Add a bio')
+    # projects
+    # contacts =
+    
+    def __str__(self):
+        return f'{self.bio}'     
 
--- projectranker/settings.py
+++ projectranker/settings.py
@@ -65,6 +65,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'django.template.context_processors.static',
+                'django.template.context_processors.media',
             ],
         },
     },
@@ -123,3 +124,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
