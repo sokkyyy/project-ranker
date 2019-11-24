3c345b5a2fd7efd4d4caca08df6edec5ef1deb21
sokkyyy <ndegwaraymond@gmail.com>
2019-11-24 16:44:26 +0300

    Added styling for the registration form

--- project/forms.py
+++ project/forms.py
@@ -1,7 +1,7 @@
 from django.forms import ModelForm
 from django.contrib.auth.models import User
-
+from django import forms
 class RegForm(ModelForm):
     class Meta:
         model = User 
-        fields = ['username','email','password']
+        fields = ['username','email','password','first_name', 'last_name']
--- projectranker/settings.py
+++ projectranker/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'project.apps.ProjectConfig',
     'django_semantic_ui',
+    'semanticuiforms',
 ]
 
 MIDDLEWARE = [
