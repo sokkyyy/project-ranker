6a3b0cbdf93c64a0b687976668f3d07c2ad8f590
sokkyyy <ndegwaraymond@gmail.com>
2019-11-24 11:00:10 +0300

    Connected project to postgresql to store application data and information.

--- projectranker/settings.py
+++ projectranker/settings.py
@@ -78,8 +78,10 @@ WSGI_APPLICATION = 'projectranker.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'projectranker',
+        'USER':'samurai',
+        'PASSWORD':'superman7577',
     }
 }
 
@@ -108,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
