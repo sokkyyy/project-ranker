b0a54cb271d8d1fe6c32a51c02a42ae497475529
sokkyyy <ndegwaraymond@gmail.com>
2019-11-24 14:42:07 +0300

    Added a form for registering users. Also added the auth folder in the templates log in and registration

--- project/views.py
+++ project/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
-
+from .forms import RegForm
 # Create your views here.
 def home(request):
     return render(request,'home.html')
+
+def register(request):
+    form = RegForm()
+    return render(request,'auth/registration.html',
+    {"form":form})
